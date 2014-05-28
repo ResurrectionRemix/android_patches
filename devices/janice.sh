b3fa00d6b382392263b6764c2d76089f5586b2e3
t4n017 <tanoabeleyra@gmail.com>
2014-05-28 17:12:59 -0300

    Replace patches with picks [1/2]

---
+++ devices/janice.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+repo="frameworks/av"
+echo -e "${bldblu}  Patching $repo .."
+cd $top_path/$repo
+$normal
+git fetch https://github.com/t4n017/android_frameworks_av.git kitkat
+git cherry-pick 2378992ff57c858c0de9c393b3b5d8457e6cf727
+
+echo ""
+echo ""
+
+repo="frameworks/base"
+echo -e "${bldblu}  Patching $repo .."
+cd $top_path/$repo
+$normal
+git fetch https://github.com/t4n017/android_frameworks_base.git kitkat
+git cherry-pick fa8f25872cfbe681b70363323cd8b9e7f963bd42
+
+echo ""
+echo ""
+
+repo="frameworks/native"
+echo -e "${bldblu}  Patching $repo .."
+cd $top_path/$repo
+$normal
+git fetch https://github.com/t4n017/android_frameworks_native.git kitkat
+git cherry-pick 508012451a6ad9066c37845a03c7665cf43ec449
+
+echo ""
+echo ""
+
+repo="hardware/libhardware_legacy"
+echo -e "${bldblu}  Patching $repo .."
+cd $top_path/$repo
+$normal
+git fetch https://github.com/t4n017/android_hardware_libhardware_legacy.git kitkat
+git cherry-pick 144266f364254659ecf798406762fdbc9bc75d52
+
+echo ""
+echo ""
+
+repo="packages/apps/Settings"
+echo -e "${bldblu}  Patching $repo .."
+cd $top_path/$repo
+$normal
+git fetch https://github.com/t4n017/android_packages_apps_Settings.git kitkat
+git cherry-pick 6eb2671a5eae60811b39d921b5308835b81abc04
+
+echo ""
+echo ""
+
+repo="packages/services/Telephony"
+echo -e "${bldblu}  Patching $repo .."
+cd $top_path/$repo
+$normal
+git fetch https://github.com/t4n017/android_packages_services_Telephony.git kitkat
+git cherry-pick 8a5862edc9d04fed26ed3d62f50691066c24d3d0
+
+echo ""
+echo ""
+
+repo="system/core"
+echo -e "${bldblu}  Patching $repo .."
+cd $top_path/$repo
+$normal
+git fetch https://github.com/t4n017/android_system_core.git kitkat
+git cherry-pick 14bff33783923978ad77a55d71e598f0eabb0a25
+git cherry-pick 7a43ef8049e87600938f64d4f241e0e5d3d282f1
+
+echo ""
+echo ""
+
+repo="system/vold"
+echo -e "${bldblu}  Patching $repo .."
+cd $top_path/$repo
+$normal
+git fetch https://github.com/t4n017/android_system_vold.git kitkat
+git cherry-pick 3af7b597586ee571d0448026050fcbfcc177f3b0
--- patch.sh
+++ patch.sh
@@ -1,20 +1,4 @@
-#    Android Patches Script
-#
-#    Copyright (C) 2014 Tano Abeleyra (@t4n017)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+#!/bin/sh
 
 # No scrollback buffer
 echo -e '\0033\0143'
@@ -30,7 +14,8 @@ normal='tput sgr0'
 
 # Initialize
 top_path="$( cd .. && pwd )"
-devices_list=(*/ "EXIT")
+cd devices
+devices_list=(*.sh "EXIT")
 
 
 # Device list
@@ -38,18 +23,18 @@ echo -e "${bldgrn}  #### Device list ####"
 echo ""
 i=1
 for device in "${devices_list[@]}";do
-    echo -e "${bldblu}  $i. $(echo $device | cut -d / -f 1)"
+    echo -e "${bldblu}  $i. $(echo $device | cut -d . -f 1)"
     let i=$i+1
 done
 
+
 echo ""
-echo "${bldgrn}  #####################"
 echo ""
 
 
 # Device selection
 read -p "${bldcya}  Choose your device: $($normal)" option
-device=${devices_list[(($option-1))]}
+device="$(echo ${devices_list[(($option-1))]} | cut -d . -f 1)"
 
 
 # User decided to exit
@@ -59,6 +44,7 @@ if [ $device == "EXIT" ];then
     echo "${bldgrn}  Bye!"
     echo ""
     echo ""
+    return 1
 fi
 
 
@@ -67,55 +53,21 @@ clear
 
 
 # Applying patches
-cd $device
-patches_path=$( pwd )
-patches=(*.patch)
-
-echo -e ""
-echo -e ""
-echo -e "${bldgrn}  #### Applying patches for $(echo $device | cut -d / -f 1) ####"
-echo -e ""
-echo -e ""
-
-sleep 2s
-
-for patch in "${patches[@]}";do
-    patch_dest="$(echo $patch | cut -d. -f1 | sed 's:_:/:g' | sed 's/@/_/g')"
-    cp $patch $top_path/$patch_dest
-    cd $top_path/$patch_dest
-    echo -e "${bldblu}  Patching $patch_dest .."
-    $normal
-    git checkout .
-    patch -N -p1 < $patch
-    rm $patch
-    cd $patches_path
-    echo -e ""
-done
+echo ""
+echo ""
+echo -e "${bldgrn}  #### Applying patches for $device ####"
+echo ""
+echo ""
+. "$device.sh"
+echo ""
+echo ""
+echo -e "${bldgrn} Patching done."
+echo ""
+echo ""
 
 
-# Back to top of patches folder
-cd ..
-
-echo -e ""
-echo -e "${bldgrn}  Patching complete.\n  Review to see if no errors were there.."
-echo -e ""
-read -p "${bldcya}  Do you want to revert patches?[y/n]:  $($normal)" option
-echo -e ""
-echo -e
-
-if [ $option == "n" ];then
-    echo -e "${bldgrn}  Patches won't be reverted\n  You should use the revert script before next repo sync."
-    cd $top_path
-    echo -e ""
-    echo -e ""
-else
-    # Reverting patches
-	clear
-    ./revert.sh $device
-    cd $top_path
-    echo -e ""
-    echo -e ""
-fi
+# Back to top
+cd $top_path
 
 
 # Switch terminal back to normal
